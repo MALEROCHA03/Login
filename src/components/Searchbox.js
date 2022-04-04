801ef11e05df8304bcdff8a542648c18b8bdadce
Alejandra Rocha <alejalafafita@gmail.com>
2022-04-04 11:42:39 -0500

    changes nav

--- src/components/Searchbox.js
+++ src/components/Searchbox.js
@@ -32,14 +32,14 @@ export const Searchbox = () => {
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="me-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#home">Inicio</Nav.Link>
           <Nav.Link href="#link">Link</Nav.Link>
-          <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+          <NavDropdown title="Dropdown" id="basic-nav-Desplegable">
+            <NavDropdown.Item href="#action/3.1">Accióm</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">Python</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.3">React</NavDropdown.Item>
             <NavDropdown.Divider />
-            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.4">Bootstrap</NavDropdown.Item>
           </NavDropdown>
         </Nav>
       </Navbar.Collapse>
